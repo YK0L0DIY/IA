03df5f77d72ea93c494e5935dd1d4ac07a0a323a
root <ykolodiy17@gmail.com>
2020-05-06 07:44:36 +0100

    estado inicial, e movimento).

--- problema4/ex1.pl
+++ problema4/ex1.pl
@@ -1,9 +1,37 @@
-tamanho(4).
+:- dynamic(tamanho/1).
+:- dynamic(estado_inicial/1).
 
+%estado inicial consoante o tamanho
+criar_estado_inicial():-
+    tamanho(T),
+    length(N, T),
+    maplist(random(1,T), N),
+    asserta(estado_inicial(N)).
+
+mover([_|[]],1,Y,[Y]).
+mover([_|T],1,Y,[Y|T]).
+
+mover([E|T],X,Y,[E|En]):-
+    X \= 1,
+    X1 is X-1,
+    mover(T,X1,Y,En).
+
+% realizar joagada
+% op(estadio_inicial, nome_jogada, rainha_a_mover, posicao_para_onde_mocer, estado_resultante)
+op(E,mover,X,Y,En) :-
+    tamanho(N),
+    X > 0,
+    N >= X,
+    Y > 0,
+    N >= Y,
+    mover(E,X,Y,En).
+
+% verificar se e estado final
 estado_final(E) :-
     tamanho(N),
     queens(N,E).
 
+% algoritmo pesquisa
 pesquisa_local_hill_climbingSemCiclos(E, _) :-
 	estado_final(E),
 	write(E), write(' ').
@@ -26,6 +54,7 @@ obtem_no([H|_], H).
 obtem_no([_|T], H1) :-
 	obtem_no(T, H1).
 
-pesquisa :-
-	estado_inicial(S0),
-	pesquisa_local_hill_climbingSemCiclos(S0, []).
+pesquisa(N) :-
+    asserta(tamanho(N)),
+	criar_estado_inicial().
+	%pesquisa_local_hill_climbingSemCiclos(S0, []).
