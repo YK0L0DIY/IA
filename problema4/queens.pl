460a796fae228ef5fcd8d2aa42f8e99e73e9b78e
Edfame <theedfame@gmail.com>
2020-05-09 09:27:51 +0100

    changed queens

--- problema4/queens.pl
+++ problema4/queens.pl
@@ -1,24 +1,16 @@
-queens(N, Queens) :-
-    length(Queens, N),
-	board(Queens, Board, 0, N, _, _),
-	queens(Board, 0, Queens).
 
-board([], [], N, N, _, _).
-board([_|Queens], [Col-Vars|Board], Col0, N, [_|VR], VC) :-
-	Col is Col0+1,
-	functor(Vars, f, N),
-	constraints(N, Vars, VR, VC),
-	board(Queens, Board, Col, N, VR, [_|VC]).
+:- use_module(library(clpfd)).
 
-constraints(0, _, _, _) :- !.
-constraints(N, Row, [R|Rs], [C|Cs]) :-
-	arg(N, Row, R-C),
-	M is N-1,
-	constraints(M, Row, Rs, Cs).
+ok([]).
+ok([R|Rs]) :- ok(Rs, R, 1), ok(Rs).
 
-queens([], _, []).
-queens([C|Cs], Row0, [Col|Solution]) :-
-	Row is Row0+1,
-	select(Col-Vars, [C|Cs], Board),
-	arg(Row, Vars, Row-Row),
-	queens(Board, Row, Solution).
+ok([], _, _).
+ok([Rj|Rs], Ri, I) :-
+    I1 is I+1,
+    ok(Rs, Ri, I1),
+    Ri #\= Rj, Ri #\= Rj+I, Ri+I #\= Rj.
+
+queens(N, R) :-
+    length(R, N),
+    R ins 1..N,
+    ok(R).
