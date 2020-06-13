efbc8e3a730d7ac9d48b6b3fee657d592097f268
Edfame <edfame@gmail.com>
2020-06-13 17:43:55 +0100

    We can play!
    TODO: Added search algorithms

--- final/src/main.py
+++ final/src/main.py
@@ -1,13 +1,25 @@
 from utils import *
-
-initial_state = {
-    'player_1': 0,
-    'player_0': 0,
-    'line_1': [4, 4, 4, 4, 4, 4],
-    'line_0': [4, 4, 4, 4, 4, 4],
-}
+import copy
 
 if __name__ == '__main__':
+
+    initial_state = {
+        'player_1': 0,
+        'player_0': 0,
+        'line_1': [4, 4, 4, 4, 4, 4],
+        'line_0': [4, 4, 4, 4, 4, 4],
+    }
+
     print_info()
     print_state(initial_state)
-    print_state(play(initial_state, 5, 1))
+    tab = copy.deepcopy(initial_state)
+    p = 0
+
+    while not final_state(tab):
+
+        pos = int(input(f"P_{p % 2}> "))
+
+        if pos_is_playable(tab, pos, p % 2):
+            tab = play(tab, pos, p % 2)
+            print_state(tab)
+            p += 1
--- final/src/utils.py
+++ final/src/utils.py
@@ -1,21 +1,39 @@
-from constants import MIN_CAPTURE, MAX_CAPTURE
+from termcolor import colored
+from constants import *
+import copy
+
+
+def pos_is_playable(state: dict, pos: int, player: int):
+    pos_value = state[f'line_{player}'][pos]
+    player_line = state[f'line_{player}']
+    opponent_line = state[f'line_{1 if player == 1 else 0}']
+
+    if pos_value == 0:
+        return False
+
+    if sum(opponent_line) > 0:
+        if pos_value == 1:
+            return max(player_line) == 1
+        return True
+
+    else:
+        return pos_value >= LINE_SIZE - pos
 
 
 def play(state: dict, pos: int, player: int):
+    result_state = copy.deepcopy(state)
+
     if player == 1:
-        number_of_elements = state['line_1'][pos]
-        state['line_1'][pos] = 0
+        number_of_elements = result_state['line_1'][pos]
+        result_state['line_1'][pos] = 0
 
     else:
-        number_of_elements = state['line_0'][pos]
-        state['line_0'][pos] = 0
+        number_of_elements = result_state['line_0'][pos]
+        result_state['line_0'][pos] = 0
 
-    result_state, last_placed = distribute(state, pos + 1, number_of_elements, player, (pos, player))
+    result_state, last_placed = distribute(result_state, pos + 1, number_of_elements, player, (pos, player))
 
     if player != last_placed['player']:
-        #print('Capturing ', last_placed['pos'], last_placed['line'], ' from: ')
-        #print_state(result_state)
-
         result_state, points = collect(result_state, last_placed)
         result_state[f'player_{player}'] += points
 
@@ -25,10 +43,10 @@ def play(state: dict, pos: int, player: int):
 def collect(state: dict, last_placed: dict):
     points = 0
 
-    for i in range(0, 6):
-        if MIN_CAPTURE <= state[last_placed['line']][last_placed['pos'] - i] <= MAX_CAPTURE:
-            points += state[last_placed['line']][last_placed['pos'] - i]
-            state[last_placed['line']][last_placed['pos'] - i] = 0
+    for i in range(last_placed['pos'], -1, -1):
+        if MIN_CAPTURE <= state[last_placed['line']][i] <= MAX_CAPTURE:
+            points += state[last_placed['line']][i]
+            state[last_placed['line']][i] = 0
         else:
             break
 
@@ -40,7 +58,7 @@ def distribute(state: dict, start: int, elements: int, player: int, inicial: tup
     last_player = None
 
     if player == 1:
-        for i in range(start, 6):
+        for i in range(start, LINE_SIZE):
             # ignore the start space
             if i == inicial[0] and player == inicial[1]:
                 continue
@@ -55,7 +73,7 @@ def distribute(state: dict, start: int, elements: int, player: int, inicial: tup
                 break
 
     else:
-        for i in range(start, 6):
+        for i in range(start, LINE_SIZE):
             # ignore the start space
             if i == inicial[0] and player == inicial[1]:
                 continue
@@ -68,21 +86,29 @@ def distribute(state: dict, start: int, elements: int, player: int, inicial: tup
                 break
 
     if elements > 0:
-        return distribute(state, 0, elements, -player, inicial)
+        return distribute(state, 0, elements, 1 if player != 1 else 0, inicial)
     return state, {'pos': last_pos, 'line': last_player, 'player': player}
 
 
+def game_over(state: dict):
+    return state['player_0'] > MAX_SCORE or state['player_1'] > MAX_SCORE
+
+
 def final_state(state: dict):
-    return False
+    if not game_over(state):
+        for i in range(0, LINE_SIZE):
+            if pos_is_playable(state, i, 0) or pos_is_playable(state, i, 1):
+                return False
+    return True
 
 
 def print_state(state: dict):
     up_line = state['line_1']
     dow_line = state['line_0']
-    print('┌P_1----------------------------┐\n' +
-          f"| {state['player_1']} | {up_line[5]} | {up_line[4]} | {up_line[3]} | {up_line[2]} | {up_line[1]} | {up_line[0]} |   |\n" +
-          f"|   | {dow_line[0]} | {dow_line[1]} | {dow_line[2]} | {dow_line[3]} | {dow_line[4]} | {dow_line[5]} | {state['player_0']} |\n" +
-          '└----------------------------P_0┘')
+    print(f"┌{colored('P_1', 'red')}----------------------------┐\n" +
+          f"| {colored(state['player_1'], 'red')} | {up_line[5]} | {up_line[4]} | {up_line[3]} | {up_line[2]} | {up_line[1]} | {up_line[0]} |   |\n" +
+          f"|   | {dow_line[0]} | {dow_line[1]} | {dow_line[2]} | {dow_line[3]} | {dow_line[4]} | {dow_line[5]} | {colored(state['player_0'], 'blue')} |\n" +
+          f"└----------------------------{colored('P_0', 'blue')}┘")
 
 
 def print_info():
