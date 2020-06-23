8617a63e7f2b8b0e94b8cf89aa1de96637940682
Edfame <theedfame@gmail.com>
2020-06-23 13:19:19 +0100

    Fully functional

--- final/src/main.py
+++ final/src/main.py
@@ -12,10 +12,16 @@ from sys import setrecursionlimit
 
 
 def print_scores(state: dict):
-    print("SCORES: P_0:", tab['player_0'], " P_1:", tab['player_1'])
 
+    if state['player_1'] > state['player_0']:
+        print("PLAYER 1 WON!")
+    elif state['player_0'] > state['player_1']:
+        print("PLAYER 0 WON!")
+    else:
+        print("IT'S A DRAW!")
 
-plays = 0
+    print("SCORES: P_0:", state['player_0'], " P_1:", state['player_1'])
+
 
 if __name__ == '__main__':
 
@@ -27,12 +33,12 @@ if __name__ == '__main__':
                         required=False)
     parser.add_argument('-t, --board', action='store_true', dest='board', help='To display the board',
                         required=False)
+    parser.add_argument('-r, --answer', action='store_true', dest='singleAnswer', help='To display a single an answer',
+                        required=False)
     parser.add_argument('-d, --difficulty', type=str, dest='difficulty',
                         help='The difficulty of the game, a -> 5s, b -> 15s and c -> 30s',
                         required=True)
-    parser.add_argument('-e, --state', type=dict, dest='state',
-                        help='The difficulty of the game, a -> 5s, b -> 15s and c -> 30s',
-                        required=False)
+
     args = parser.parse_args()
 
     # funciona como um dicionario, se -p ent args.first vai estar a true e se -s ent args.second vai estar a true
@@ -53,68 +59,93 @@ if __name__ == '__main__':
         'line_0': [4, 4, 4, 4, 4, 4],
     }
 
-    print_info()
     print_state(initial_state)
     tab = copy.deepcopy(initial_state)
+    player0_plays = 0
+    player1_plays = 0
 
     print(args)
 
-    if args.first:
-        p = 1
-    elif args.second:
-        p = 0
-    else:
-        p = 0
+    if args.singleAnswer:
 
-    if args.difficulty == 'a':
-        difficulty = FIVE_SECONDS_ALPHABETA
-    elif args.difficulty == 'b':
-        difficulty = FIFTEEN_SECONDS_ALPHABETA
-    elif args.difficulty == 'c':
-        difficulty = THIRTY_SECONDS_ALPHABETA
+        if args.difficulty == 'a':
+            difficulty = FIVE_SECONDS_ALPHABETA_SA
+        elif args.difficulty == 'b':
+            difficulty = FIFTEEN_SECONDS_ALPHABETA_SA
+        elif args.difficulty == 'c':
+            difficulty = THIRTY_SECONDS_ALPHABETA_SA
 
-    while not final_state(tab):
+        start = time.time()
 
-        if p % 2 == 0:
-            start = time.time()
-            if plays == 0:
-                pos = randint(0, 5)
-                plays += 1
-            else:
-                pos = minimax(tab, THIRTY_SECONDS_MINIMAX)
+        pos = alphabeta(tab, difficulty)
 
-            end = time.time()
-            print('Evaluation time: {}s'.format(round(end - start, 7)))
+        end = time.time()
+        print('Evaluation time: {}s'.format(round(end - start, 7)))
 
-            print("P_0 PLAYED: ", pos + 1)
+        tab = play(tab, pos, PLAYER_1)
 
-        else:
-
-            start = time.time()
-
-            if plays == 1:
-                pos = randint(0, 5)
-                plays += 1
-            else:
-                pos = alphabeta(tab, THIRTY_SECONDS_ALPHABETA)
-
-            end = time.time()
-            print('Evaluation time: {}s'.format(round(end - start, 7)))
-
-            print("P_1 PLAYED: ", pos + 1)
-
-        tab = play(tab, pos, p % 2)
-
-        if args.board:
-            print_state(tab)
-        p += 1
-
-        if tab['player_1'] > tab['player_0']:
-            print("PLAYER 1 WON!")
-        elif tab['player_0'] > tab['player_1']:
-            print("PLAYER 0 WON!")
-        else:
-            print("IT'S A DRAW!")
-
-        print_scores(tab)
         print_state(tab)
+        print("P_1 PLAYED: ", pos + 1)
+
+    else:
+
+        if args.difficulty == 'a':
+
+            difficulty = FIVE_SECONDS_ALPHABETA
+            m_difficulty = FIVE_SECONDS_MINIMAX
+
+        elif args.difficulty == 'b':
+
+            difficulty = FIFTEEN_SECONDS_ALPHABETA
+            m_difficulty = FIFTEEN_SECONDS_MINIMAX
+
+        elif args.difficulty == 'c':
+
+            difficulty = THIRTY_SECONDS_ALPHABETA
+            m_difficulty = THIRTY_SECONDS_MINIMAX
+
+        if args.first:
+            p = 1
+        elif args.second:
+            p = 0
+        else:
+            p = 0
+
+        while not final_state(tab):
+
+            if p % 2 == 0:
+                start = time.time()
+                if player0_plays == 0:
+                    pos = randint(0, 5)
+                    player0_plays += 1
+                else:
+                    pos = minimax(tab, m_difficulty)
+
+                end = time.time()
+                print('Evaluation time: {}s'.format(round(end - start, 7)))
+
+                print("P_0 PLAYED: ", pos + 1)
+
+            else:
+
+                start = time.time()
+
+                if player1_plays == 0:
+                    pos = randint(0, 5)
+                    player1_plays += 1
+                else:
+                    pos = alphabeta(tab, difficulty)
+
+                end = time.time()
+                print('Evaluation time: {}s'.format(round(end - start, 7)))
+
+                print("P_1 PLAYED: ", pos + 1)
+
+            tab = play(tab, pos, p % 2)
+
+            if args.board:
+                print_state(tab)
+            p += 1
+
+        print_state(tab)
+        print_scores(tab)
